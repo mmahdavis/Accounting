0eb8a39c6b01b6f8adaabfb865a42acdcde91931
mmahdavis <mmahdavis20@gmail.com>
2024-01-29 11:59:45 +0330

    add can AccessPanel to user

--- app/Models/User.php
+++ app/Models/User.php
@@ -71,6 +71,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@mmahdavis.ir') && $this->email == 'mmahdavis20@gmail.com';
+        return str_ends_with($this->email, '@mmahdavis.ir') || $this->email == 'mmahdavis20@gmail.com';
     }
 }
